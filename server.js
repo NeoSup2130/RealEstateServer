3404a1e859108b78be2738ce56e435e5171fee21
NeoSup2130 <slavaxbox@hotmail.nl>
2023-11-02 20:39:57 +0100

    [+] Added cors to whitelist development and production website.

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const jsonServer = require('json-server');
 const auth = require('json-server-auth')
+const cors = require('cors');
 
 const app  = jsonServer.create();
 const router = jsonServer.router('db.json');
+
+const whitelist = ["https://localhost:8000", 'https://slava-real-estate-care.netlify.app']
+
+app.use(cors({
+    origin : (origin, callback) =>
+    {
+        if (whitelist.indexOf(origin) !== -1) 
+        {
+          callback(null, true)
+        } 
+        else 
+        {
+          callback(new Error('Not allowed by CORS'))
+        }
+    },
+    optionsSuccessStatus: 200
+}))
 
 app.db = router.db;
 
